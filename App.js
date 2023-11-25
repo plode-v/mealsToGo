376718fba7b139f9a23232c92b5df214f28fcf35
Plode <84858705+plode-v@users.noreply.github.com>
2023-11-25 08:08:28 +0700

    setup homepage

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform, SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Our react native blank slate</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: Platform.OS === "android" && 10,
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+
+  }
 });
